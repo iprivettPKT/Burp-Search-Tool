d4a614273c76309a418695c24d2fcb80bc939c97
iprivettPKT <144707717+iprivettPKT@users.noreply.github.com>
2023-10-25 09:52:17 -0600

    Update burpsearch.py

--- burpsearch.py
+++ burpsearch.py
@@ -14,28 +14,67 @@ class XMLSearchApp(wx.Frame):
         
         self.InitUI()
 
+        # Adjust the initial size of the window
+        self.SetSize((1200, 1000))
+
     def InitUI(self):
         pnl = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
-        
+
         st1 = wx.StaticText(pnl, label='Enter Search Term:')
         vbox.Add(st1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
+
         self.search_term = wx.TextCtrl(pnl)
         vbox.Add(self.search_term, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
+
         self.browse_btn = wx.Button(pnl, label='Browse')
         self.browse_btn.Bind(wx.EVT_BUTTON, self.OnBrowse)
         vbox.Add(self.browse_btn, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
+
         self.start_search_btn = wx.Button(pnl, label='Start Search')
-        self.start_search_btn.Bind(wx.EVT_BUTTON, self.OnSearch)
+        self.start_search_btn.Bind(wx.EVT_BUTTON, self.SearchClick)
         vbox.Add(self.start_search_btn, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-        
+
+        # Add the 'Generated Files' section and its related widgets
+        st2 = wx.StaticText(pnl, label='Generated Files:')
+        vbox.Add(st2, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
+        self.file_listbox = wx.ListBox(pnl, style=wx.LB_SINGLE)
+        vbox.Add(self.file_listbox, proportion=1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
+        self.refresh_btn = wx.Button(pnl, label='Refresh List')
+        self.refresh_btn.Bind(wx.EVT_BUTTON, self.OnRefresh)
+        vbox.Add(self.refresh_btn, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
+        self.open_btn = wx.Button(pnl, label='Open Selected File')
+        self.open_btn.Bind(wx.EVT_BUTTON, self.OnOpen)
+        vbox.Add(self.open_btn, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
+        # Define the horizontal box sizer for the request and response areas
+        hbox_listboxes = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Request display area
+        self.request_display = wx.TextCtrl(pnl, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        hbox_listboxes.Add(self.request_display, proportion=2, flag=wx.EXPAND|wx.RIGHT, border=5)
+
+        # Response display area
+        self.response_display = wx.TextCtrl(pnl, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        hbox_listboxes.Add(self.response_display, proportion=2, flag=wx.EXPAND|wx.LEFT, border=5)
+
+        # Add the horizontal box containing the request and response areas to the vertical box sizer
+        vbox.Add(hbox_listboxes, proportion=2, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
         pnl.SetSizer(vbox)
-        
-        self.SetTitle('XML Search Tool')
+
+        self.SetTitle('Burp Bulk Search Tool')
         self.Centre()
+
+    def SearchClick(self, event):
+        # Execute first function
+        self.OnSearch(event)
+        
+        # Execute second function
+        self.OnRefresh(event)
 
     def OnBrowse(self, event):
         openFileDialog = wx.FileDialog(self, "Open XML file", "", "", "XML files (*.xml)|*.xml", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
@@ -43,6 +82,55 @@ class XMLSearchApp(wx.Frame):
             return
         self.file_path = openFileDialog.GetPath()
         self.browse_btn.SetLabel(self.file_path.split(os.path.sep)[-1])
+
+    def OnRefresh(self, event):
+        # Clear the current list
+        self.file_listbox.Clear()
+            
+        # Get the search term to identify the results directory
+        term = self.search_term.GetValue()
+        main_dir = "Burp_Search_Results"
+        results_dir = "results_" + term
+        dir_path = str(main_dir + "/" + results_dir)
+
+        if os.path.exists(dir_path):
+
+            # Extract the number from the filename for sorting
+            def sort_key(filename):
+                return int(filename.split("_")[1].split(".")[0])
+            
+            files = sorted(os.listdir(dir_path), key=sort_key)
+            # List files in results directory
+            for filename in files:
+                if filename.endswith(".txt"):
+                    self.file_listbox.Append(filename)
+
+    def OnOpen(self, event):
+        self.request_display.Clear()
+        self.response_display.Clear()
+        # Get selected file from listbox
+        selected_file = self.file_listbox.GetString(self.file_listbox.GetSelection())
+    
+        term = self.search_term.GetValue()
+        main_dir = "Burp_Search_Results"
+        results_dir = "results_" + term
+        dir_path = str(main_dir + "/" + results_dir + "/" + selected_file)
+        #full_path = os.path.join(results_dir, selected_file)
+
+        # Read and display the contents
+        with open(dir_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            request_part = content.split("----- REQUEST -----")[1].split("----- RESPONSE -----")[0].strip()
+            response_part = content.split("----- RESPONSE -----")[1].strip()                                  
+        self.request_display.AppendText(request_part + "\n\n")
+        self.response_display.AppendText(response_part + "\n\n")
+
+        # Scroll back to the top
+        self.request_display.SetInsertionPoint(0)
+        self.response_display.SetInsertionPoint(0)
+
+        self.highlight_search_term(self.request_display, term)
+        self.highlight_search_term(self.response_display, term)
 
     def OnSearch(self, event):
         term = self.search_term.GetValue()
@@ -93,12 +181,27 @@ class XMLSearchApp(wx.Frame):
         # After loop, print total matches
         print(f"Total matches: {file_counter}")
 
+    def highlight_search_term(self, textctrl, term):
+        start_pos = 0
+        term_length = len(term)
 
+        # Define the highlight style (yellow background)
+        highlight_style = wx.TextAttr(wx.BLACK, wx.YELLOW)
+
+        # Find and highlight all occurrences of the search term
+        content = textctrl.GetValue()
+        while start_pos < len(content):
+            start_pos = content.find(term, start_pos)
+            if start_pos == -1:
+                break
+            textctrl.SetStyle(start_pos, start_pos + term_length, highlight_style)
+            start_pos += term_length
 
 if __name__ == '__main__':
     app = wx.App()
     frm = XMLSearchApp(None, title='XML Search Tool', size=(400, 200))
     frm.Show()
+    app.MainLoop()
     app.MainLoop()
 
 
